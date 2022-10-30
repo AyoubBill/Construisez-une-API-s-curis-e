48cb5a34a96e0b82a6582807c82667089125265b
Ayoub Bil <bilayoub@gmail.com>
2022-10-30 11:43:17 +0100

    Construisez une API sécurisée pour une application d'avis gastronomiques

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://ayoub:8dwfPkxJKJFU7O9P@cluster0.dms4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+
+module.exports = app;
